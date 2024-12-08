4a93931f8696af522f20a9f77865a45fbf2cf69e
Tadej Mali <tadej.mali@gmail.com>
2024-12-08 15:51:11 +0100

    Adding some integration tests.

---
+++ src/test/java/io/licitat/impl/ScoreboardImplIntegrationTest.java
@@ -0,0 +1,84 @@
+package io.licitat.impl;
+
+import io.licitat.soccer.Match;
+import io.licitat.soccer.Score;
+import io.licitat.test_data.TeamId;
+import io.licitat.test_data.TestMatchFactory;
+import io.licitat.test_data.TestTeamFactory;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreboardImplIntegrationTest {
+
+    private final Comparator<Match> displayOrder = new MatchSorter();
+
+    @Test
+    public void givenNewMatch_startMatch_newMatchCreated() {
+        var board = new ScoreboardImpl(new MatchRepositoryImpl(), displayOrder);
+
+        var newMatch = board.StartMatch(
+            TestTeamFactory.getById(TeamId.Germany),
+            TestTeamFactory.getById(TeamId.Argentina)
+        );
+
+        assertEquals(newMatch.getHomeTeam().id(), TeamId.Germany.getValue());
+        assertEquals(newMatch.getAwayTeam().id(), TeamId.Argentina.getValue());
+        assertEquals(0, newMatch.getHomeScore());
+        assertEquals(0, newMatch.getAwayScore());
+    }
+
+    @Test
+    public void givenNewScore_updateScore_scoreUpdated() {
+        var board = new ScoreboardImpl(new MatchRepositoryImpl(), displayOrder);
+
+        var currentMatch = board.StartMatch(
+            TestTeamFactory.getById(TeamId.Uruguay),
+            TestTeamFactory.getById(TeamId.Spain)
+        );
+
+        var newScore = new Score(
+            currentMatch.getHomeTeam().id(), 2,
+            currentMatch.getAwayTeam().id(), 1
+        );
+        var updatedMatch = board.UpdateScore(newScore);
+
+        assertEquals(currentMatch.getHomeTeam().id(), updatedMatch.getHomeTeam().id());
+        assertEquals(currentMatch.getAwayTeam().id(), updatedMatch.getAwayTeam().id());
+        assertEquals(2, updatedMatch.getHomeScore());
+        assertEquals(1, updatedMatch.getAwayScore());
+    }
+
+    @Test
+    public void givenMatchOver_finishMatch_matchRemoved() {
+        var board = new ScoreboardImpl(new MatchRepositoryImpl(), displayOrder);
+
+        var currentMatch = board.StartMatch(
+            TestTeamFactory.getById(TeamId.Uruguay),
+            TestTeamFactory.getById(TeamId.Spain)
+        );
+
+        board.FinishMatch(currentMatch);
+
+        var isNoLongerActive = board.GetActiveMatches().stream()
+            .noneMatch(m ->
+                m.getHomeTeam().isTheSameTeam(currentMatch.getHomeTeam())
+                && m.getAwayTeam().isTheSameTeam(currentMatch.getAwayTeam())
+            );
+
+        assertTrue(isNoLongerActive);
+    }
+
+    @Test
+    public void givenActiveMatches_getActiveMatches_getSortedListOfMatches() {
+        var board = new ScoreboardImpl(new MatchRepositoryImpl(), displayOrder);
+
+        var allMatches = TestMatchFactory.buildMatches();
+        allMatches.forEach(m -> board.StartMatch(m.getHomeTeam(), m.getAwayTeam()));
+
+        var orderedMatches = board.GetActiveMatches();
+        assertEquals(allMatches.size(), orderedMatches.size());
+    }
+}
