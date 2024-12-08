b40e6a1d7bf4d57bdb20efe65297656f550951aa
Tadej Mali <tadej.mali@gmail.com>
2024-12-08 15:32:47 +0100

    Additional roboustness tests

--- src/test/java/io/licitat/impl/MatchRepositoryImplTest.java
+++ src/test/java/io/licitat/impl/MatchRepositoryImplTest.java
@@ -53,11 +53,43 @@ public class MatchRepositoryImplTest {
     }
 
     @Test
+    public void givenMatchIsNotActive_updateMatch_shallAssert() {
+        var repo = new MatchRepositoryImpl();
+        var initialMatch = buildNewMatch(TeamId.Brazil, TeamId.Italy);
+
+        repo.AddMatch(initialMatch);
+
+        var updatedScore = new Score(
+            initialMatch.getHomeTeam().id(), 1,
+            initialMatch.getAwayTeam().id(), 3
+        );
+
+        repo.RemoveMatch(initialMatch);
+        var updatedMatch = initialMatch.updateScore(updatedScore);
+
+        assertThrows(AssertionError.class, () -> repo.UpdateMatch(updatedMatch));
+    }
+
+    @Test
     public void givenMatchInProgress_removeMatch_matchRemoved() {
         var repo = new MatchRepositoryImpl();
         var activeMatch = buildNewMatch(TeamId.Brazil, TeamId.Italy);
         repo.AddMatch(activeMatch);
 
+        repo.RemoveMatch(activeMatch);
+
+        var persisted = repo.FindMatch(TeamId.Brazil.getValue(), TeamId.Italy.getValue());
+        // One might argue that another match might be inserted in the meantime.
+        // However, this is a unit test and such situation would not happen.
+        assertTrue(persisted.isEmpty());
+    }
+
+    @Test
+    public void givenMatchIsNotActive_removeMatch_shallAssert() {
+        var repo = new MatchRepositoryImpl();
+        var activeMatch = buildNewMatch(TeamId.Brazil, TeamId.Italy);
+
+        repo.AddMatch(activeMatch);
         repo.RemoveMatch(activeMatch);
 
         var persisted = repo.FindMatch(TeamId.Brazil.getValue(), TeamId.Italy.getValue());
